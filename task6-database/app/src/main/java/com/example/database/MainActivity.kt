f132fa88e83974a3ff1cdbac365d2d36fc4bf9ee
Aleksandra Grygorczyk <ola.grygorczyk@gmail.com>
2023-12-17 22:53:45 +0100

    added buttons for retrieving data from Firestore collections

--- task6-database/app/src/main/java/com/example/database/MainActivity.kt
+++ task6-database/app/src/main/java/com/example/database/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.database.models.Cart
 import com.example.database.models.Category
 import com.example.database.models.Product
@@ -20,10 +22,88 @@ import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
 class MainActivity : ComponentActivity() {
+
+    private val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            DatabaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column {
+                            Button(onClick = {
+                                db.collection("categories")
+                                    .get()
+                                    .addOnSuccessListener { result ->
+                                        for (document in result) {
+                                            Log.d("Firestore", "${document.id} => ${document.data}")
+                                        }
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Log.w("Firestore", "Error getting documents.", exception)
+                                    }
+                            }) {
+                                Text(text = "Get Category data in LogCat")
+                            }
 
-        val db = Firebase.firestore
+                            Button(onClick = {
+                                db.collection("products")
+                                    .get()
+                                    .addOnSuccessListener { result ->
+                                        for (document in result) {
+                                            Log.d("Firestore", "${document.id} => ${document.data}")
+                                        }
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Log.w("Firestore", "Error getting documents.", exception)
+                                    }
+                            }) {
+                                Text(text = "Get Products data in LogCat")
+                            }
+
+                            Button(onClick = {
+                                db.collection("users")
+                                    .get()
+                                    .addOnSuccessListener { result ->
+                                        for (document in result) {
+                                            Log.d("Firestore", "${document.id} => ${document.data}")
+                                        }
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Log.w("Firestore", "Error getting documents.", exception)
+                                    }
+                            }) {
+                                Text(text = "Get Users data in LogCat")
+                            }
+
+                            Button(onClick = {
+                                db.collection("cart")
+                                    .get()
+                                    .addOnSuccessListener { result ->
+                                        for (document in result) {
+                                            Log.d("Firestore", "${document.id} => ${document.data}")
+                                        }
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Log.w("Firestore", "Error getting documents.", exception)
+                                    }
+                            }) {
+                                Text(text = "Get Cart data in Logcat")
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
 
         val category = Category(
             id = 100,
@@ -86,47 +166,5 @@ class MainActivity : ComponentActivity() {
             .addOnFailureListener { e ->
                 Log.w("Firestore", "Error adding document", e)
             }
-
-        // Reading data
-        db.collection("categories")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d("Firestore", "${document.id} => ${document.data}")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w("Firestore", "Error getting documents.", exception)
-
-
-
-                setContent {
-                    DatabaseTheme {
-                        // A surface container using the 'background' color from the theme
-                        Surface(
-                            modifier = Modifier.fillMaxSize(),
-                            color = MaterialTheme.colorScheme.background
-                        ) {
-                            Greeting("Android")
-                        }
-                    }
-                }
-            }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DatabaseTheme {
-        Greeting("Android")
     }
 }
