af30b42892c09c85092f99501acf8c3a75ed0056
Aleksandra Grygorczyk <ola.grygorczyk@gmail.com>
2024-01-06 23:03:13 +0100

    app setup and main screen layout

---
+++ task8-login/app/src/main/java/com/example/login/ui/LoginScreen.kt
@@ -0,0 +1,108 @@
+package com.example.login.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.login.R
+import com.example.login.ui.theme.LoginTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun LoginScreen() {
+    LoginTheme {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(stringResource(R.string.email)) },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Blue,
+                    unfocusedBorderColor = Color.Gray
+                )
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(stringResource(R.string.password)) },
+                visualTransformation = PasswordVisualTransformation(),
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = { TODO("handle email login") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+            ) {
+                Text(stringResource(R.string.log_in_with_email))
+            }
+
+            Button(onClick = { TODO("handle google login") },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.google_icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp).padding(end = 8.dp)
+                )
+                Text(stringResource(R.string.log_in_with_google), color = Color.Black)
+            }
+
+            Button(onClick = { TODO("handle facebook login") },
+                colors = ButtonDefaults.buttonColors(Color(0xFF1877F2)),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.facebook_icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp).padding(end = 8.dp)
+                )
+                Text(stringResource(R.string.log_in_with_facebook), color = Color.White)
+            }
+
+            TextButton(onClick = { TODO("go to signup screen") }) {
+                Text(stringResource(R.string.don_t_have_an_account_sign_up), fontSize = 16.sp)
+            }
+        }
+    }
+}
